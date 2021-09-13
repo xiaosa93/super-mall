0e21af846627f604fd2e31cf2c119aacaa7cd3f5
潇洒啊 <84223945+xiaosa-93@users.noreply.github.com>
2021-09-13 09:09:55 +0800

    初始化项目

---
+++ src/network/request.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+export function request(config) {
+  // 1.创建axios实例
+  const instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000',
+    timeout: 5000
+  })
+  // 发送真正的网络请求
+  return instance(config)
+}
